b56279ea4e3592dc5a4dbd85fe923c116fa026f8
Kuludu <i@kuludu.net>
2022-09-29 08:38:06 +0800

    Add cookies.

--- src/components/QRCode.vue
+++ src/components/QRCode.vue
@@ -2,7 +2,7 @@
   <div>
     <b-container class="align-middle">
       <b-row>
-        <b-form-input v-model="user_id" placeholder="请输入用户ID" class="mb-3"></b-form-input>
+        <b-form-input v-model="user_id" placeholder="请输入ID(user_这段是ID_这段是时间)" class="mb-3"></b-form-input>
       </b-row>
       <b-row>
         <b class="mb-3">二维码内容值：{{ qr_content }}</b>
@@ -10,8 +10,8 @@
       <b-row>
         <b-button @click="start" class="mb-3">开始模拟二维码(强制刷新)</b-button>
       </b-row>
-      <b-row>
-        <b-col>
+      <b-row class="justify-content-md-center">
+        <b-col class="col-md-auto">
           <div id="qrcode"></div>
         </b-col>
       </b-row>
@@ -21,6 +21,7 @@
 
 <script>
 import QRCode from 'qrcodejs2'
+import { useCookies } from "vue3-cookies";
 
 export default {
   name: "QRCode",
@@ -32,11 +33,21 @@ export default {
       qrcode: null
     }
   },
+  setup() {
+    const { cookies } = useCookies();
+    return { cookies };
+  },
   mounted() {
-    this.qrcode = new QRCode(document.getElementById("qrcode"))
+    this.user_id = this.cookies.get("user_id");
+    this.qrcode = new QRCode(document.getElementById("qrcode"), {
+      colorDark : "#53a351",
+      width: 256,
+      height: 256
+    })
   },
   methods: {
     start() {
+      this.cookies.set("user_id", this.user_id)
       this.update()
       clearInterval(this.timer)
       this.timer = setInterval(this.update, 7000)
