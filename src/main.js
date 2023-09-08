4d4b1d2fd729f69087acf078c6cfd9b6f10483a4
Yuxiang Fan <opensource@kuludu.net>
2023-09-08 14:41:32 +0800

    Add PWA support.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import BootstrapVue3 from 'bootstrap-vue-3'
+import BootstrapVueNext from 'bootstrap-vue-next'
 import { createRouter, createWebHistory } from 'vue-router'
 import QRCode from '@/components/QRCode'
 import JNSim from '@/components/JNSim'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import './registerServiceWorker'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -27,7 +28,7 @@ const router = createRouter({
 })
 
 const app = createApp(App)
-app.use(BootstrapVue3)
+app.use(BootstrapVueNext)
 app.use(router)
 router.isReady().then(() => {
     app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'HackJNDoor',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
 })
